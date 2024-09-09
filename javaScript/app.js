0fe2ed842a0d2dcb61237e1ddef76b7204d0fadd
William Alexander Matallana <wmatallana28@gmail.com>
2024-09-09 07:53:12 -0500

    Clase JavaScript

---
+++ javaScript/app.js
@@ -0,0 +1,57 @@
+
+// Comentario de una linea
+
+/**
+ * Comentario multilinea
+ */
+
+console.log("Hola Mundo");
+
+// Variables
+
+let numero1 = 45;
+let numero2 = 39;
+let suma, resta, multiplicacion;
+
+
+const pi = 3.14156;
+
+let estado = true;
+
+let numero3=78;
+
+let direccion = "CALLE 11 # 40-69";
+
+
+numero3 = 125;
+
+
+console.log(numero1);
+console.log(numero2);
+console.log(pi);
+console.log(estado);
+console.log(numero3);
+console.log(direccion);
+
+
+suma = numero1 + numero2;
+resta = numero1 - numero2;
+multiplicacion = numero1 * numero2;
+
+
+console.log(suma)
+
+
+// Concatenar valores
+
+console.log('EL PRIMER VALOR ES: ' + numero1);
+console.log('EL SEGUNDO VALOR ES: ' + numero2);
+console.log('LA SUMA DE ' + numero1 + ' Y ' + numero2 +' ES: ' + suma);
+console.log('LA SUMA DE ' , numero1 , ' Y ' , numero2 ,' ES: ' , suma);
+
+// Literal Template
+console.log(`LA SUMA DE ${numero1} Y ${numero2} ES: ${suma}`);
+console.log(`LA RESTA DE ${numero1} Y ${numero2} ES: ${resta}`);
+console.log(`LA MULTIPLICACION DE ${numero1} Y ${numero2} ES: ${multiplicacion}`);
+
+
